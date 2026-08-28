2d6ec1b500ce5168b05bc5dce6773d9f84ebce53
Jesús Rocha <enriquerocha2345@gmail.com>
2026-08-27 23:14:42 -0500

    Agrega reporte de detalle con columnas alineadas, cantidad de productos y totales con formato

--- Semana02/carrito/src/main/java/com/rocha/carrito/Carrito.kt
+++ Semana02/carrito/src/main/java/com/rocha/carrito/Carrito.kt
@@ -23,7 +23,21 @@ fun calcularTotal(subtotal: Double, igv: Double): Double {
     var total = subtotal + igv
     return total
 }
-
+// Función para mostrar el detalle del carrito
+fun mostrarDetallesCarrito(productos: List<Producto>) {
+    println("-----------DETALLE DEL CARRITO-----------")
+    // Variable para numerar cada producto en la lista (lo inicilizamos desde 1)
+    var i = 1
+    for (p in productos) {
+        // Calculamos el importe total de cada producto
+        val importe = p.precio * p.cantidad
+        println(String.format("%d. %-20s x%d S/ %8.2f", i,
+            p.nombre, p.cantidad, importe))
+        // Aumentar el contador para el siguiente producto
+        i++
+    }
+    println("-----------------------------------------")
+}
 fun main() {
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
@@ -42,10 +56,15 @@ fun main() {
     for (producto in carrito) {
         println("Producto agregado: ${producto.nombre}")
     }
+    // Usamos las funciones creadas para calcular el subtotal, igv y total del carrito
     val subtotal = calcularSubtotal(carrito)
     val igv = calcularIGV(subtotal)
     val total = calcularTotal(subtotal, igv)
-    println("Subtotal: S/$subtotal")
-    println("IGV 18%: S/$igv")
-    println("Total: S/$total")
+    // Mostramos el detalle del carrito
+    mostrarDetallesCarrito(carrito)
+    println("Cantidad de productos: ${carrito.size}")
+    println("")
+    println(String.format("Subtotal: S/ %8.2f", subtotal))
+    println(String.format("IGV 18%%: S/ %8.2f", igv))
+    println(String.format("TOTAL A PAGAR: S/ %8.2f", total))
 }
