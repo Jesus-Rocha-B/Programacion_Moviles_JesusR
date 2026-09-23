39074c0ef58ef2d63f4ba8d7dce17a521bba04b6
Jesús Rocha <enriquerocha2345@gmail.com>
2026-09-23 00:23:08 -0500

    Se implementó la pantalla de perfil con opción de volver al inicio limpiar el stack (todo en el ProfileScree.kt)

--- Semana05/Laboratorio/app/src/main/java/com/rocha/navlab/ui/screens/ProfileScreen.kt
+++ Semana05/Laboratorio/app/src/main/java/com/rocha/navlab/ui/screens/ProfileScreen.kt
@@ -1,1 +1,47 @@
 package com.rocha.navlab.ui.screens
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.rocha.navlab.navigation.Screen
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    // El contenido de la pantalla se centra
+    Column(
+        modifier = Modifier.fillMaxSize().padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Mi Perfil",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Jesús Rocha",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        // Botón para volver al inicio, además limpia el stack (historial)
+        Button(
+            onClick = {
+                navController.navigate(Screen.Home.route) {
+                    // Borra los Home anteriores, evita duplicados
+                    popUpTo(Screen.Home.route) {
+                        inclusive = true
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ir al inicio")
+        }
+    }
+}
