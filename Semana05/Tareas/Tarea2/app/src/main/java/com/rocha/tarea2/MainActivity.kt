b1232c66e621aabeb57886b2c0d45b6dfdb02f78
Jesús Rocha <enriquerocha2345@gmail.com>
2026-09-23 18:44:46 -0500

    Se implementó MainActivity.kt conectando el tema del proyecto con AppNavigation

--- Semana05/Tareas/Tarea2/app/src/main/java/com/rocha/tarea2/MainActivity.kt
+++ Semana05/Tareas/Tarea2/app/src/main/java/com/rocha/tarea2/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.rocha.tarea2
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.rocha.tarea2.navigation.AppNavigation
 import com.rocha.tarea2.ui.theme.Tarea2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +12,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tarea2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tarea2Theme {
-        Greeting("Android")
     }
 }
