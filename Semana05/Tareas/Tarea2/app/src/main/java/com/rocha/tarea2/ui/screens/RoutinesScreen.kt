d433eda6dc18289574df112b87888cf73b3fb6cc
Jesús Rocha <enriquerocha2345@gmail.com>
2026-09-23 18:36:14 -0500

    Se implementó RoutinesScreen.kt con un título simple

--- Semana05/Tareas/Tarea2/app/src/main/java/com/rocha/tarea2/ui/screens/RoutinesScreen.kt
+++ Semana05/Tareas/Tarea2/app/src/main/java/com/rocha/tarea2/ui/screens/RoutinesScreen.kt
@@ -1,2 +1,20 @@
 package com.rocha.tarea2.ui.screens
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
+@Composable
+fun RoutinesScreen(navController: NavController) {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(
+            text = "Rutinas",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
